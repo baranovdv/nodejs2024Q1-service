a9b2a8a16bcc3c36746481ac325f08bbb2044ca6
baranovdv <baranov.d.v.1988@gmail.com>
2024-03-11 23:44:37 +0700

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as yaml from 'yamljs';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
 
 dotenv.config();
 
@@ -10,6 +13,12 @@ const PORT = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const swagger = readFileSync('doc/api.yaml', { encoding: 'utf-8' });
+  const parsedSwagger = yaml.parse(swagger);
+
+  SwaggerModule.setup('doc', app, parsedSwagger);
+
   await app.listen(PORT);
 }
 bootstrap();
