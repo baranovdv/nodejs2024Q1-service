7fdbc41ea62abea7a93f0003a4606deaf2105f43
baranovdv <baranov.d.v.1988@gmail.com>
2024-03-25 02:48:25 +0500

    refactor: refactor seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,24 +2,7 @@ import { PrismaClient, User } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const users: User[] = [
-  {
-    id: '6f83ca43-d499-4e9d-87d2-8ce648b08d45',
-    login: 'userlogin',
-    password: 'password',
-    version: 1,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: '6f83ca43-d499-4e9d-87d2-8ce648b08d22',
-    login: 'userlogin2',
-    password: 'password2',
-    version: 1,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
+const users: User[] = [];
 
 async function seedDB() {
   for (const user of users) {
